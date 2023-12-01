56d1087d1e77c747ec95fa418cbbe806670eb155
HongMinYeong <minyung1240@daum.net>
2023-12-01 14:34:19 +0900

    Study: 스프링 부트 API POST 전송

--- src/main/java/sesac/sesacspringboot/controller/MainController.java
+++ src/main/java/sesac/sesacspringboot/controller/MainController.java
@@ -2,17 +2,16 @@ package sesac.sesacspringboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller //처음에 컨트롤러 어노테이션
+//@RestController : Controller 면서 모든 메소드가 @ResponseBody를 갖도록 하는 친구
 public class MainController {
     @GetMapping("/api") //api라는 메소드 만듬
     public String getApi(){return "request";}
 
     //1) get : ?key=value
+    // 검색/ 해시태그
     // /get/response1?name=abc
     @GetMapping("/get/response1")
 //    public String getResponse(@RequestParam(value="name") String name, Model model){
@@ -70,6 +69,30 @@ public class MainController {
         model.addAttribute("name",name);
         model.addAttribute("age",age);
         return "response";
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // post 로 값을 전달할 때 그 값을 controller에서 받는 방법은 @RequestParam
+    @PostMapping("/post/response1")
+    public String postResponse1(@RequestParam(value="name") String name, Model model){
+        model.addAttribute("name",name);
+        return "response";
+    }
+
+    @PostMapping("/post/response2")
+    public String postResponse2(@RequestParam(value="name",required = false) String name, Model model){
+        model.addAttribute("name",name);
+        return "response";
+    }
+
+    @PostMapping("/post/response3")
+    @ResponseBody // res.send
+    // return 하는 문자열의 template 파일을 불러오는 게 아니라
+    // return 하는 문자열 그대로 값을 전달하는 것
+    public String postResponse3(@RequestParam(value="name",required = false) String name, Model model){
+        model.addAttribute("name",name);
+        return "response" + name;
     }
 
 
